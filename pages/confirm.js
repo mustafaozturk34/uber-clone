a03a3f0ff6adabb5d5ee82d0eda6c333cd0e8c90
Mustafa Öztürk <mustafa.ozturk3408@gmail.com>
2021-11-08 14:44:00 +0300

    Get Coordinates from a location

--- pages/confirm.js
+++ pages/confirm.js
@@ -1,9 +1,46 @@
-import React from 'react'
+import { useEffect } from 'react'
 import tw from "tailwind-styled-components"
 import Map from './components/Map'
 
 
 const Confirm = () => {
+
+    const getPickupCoordinates = () => {
+
+        const pickup = "Santa Monica";
+        fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${pickup}.json?` +
+            new URLSearchParams({
+                access_token: "pk.eyJ1IjoibXVzdGFmYW96dHVyazM0IiwiYSI6ImNrdm1iNXd2dTA5NWIycGpwYWJrbWt5Y2kifQ.TtejJBtK0J0XU85fgsO6eg",
+                limit: 1
+            })
+        )
+        .then(response => response.json())
+        .then(data => {
+            console.log(data.features[0].center);
+        })
+    }
+
+    const getDropoffCoordinates = () => {
+
+        const dropoff = "Los Angeles";
+        fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${dropoff}.json?` +
+            new URLSearchParams({
+                access_token: "pk.eyJ1IjoibXVzdGFmYW96dHVyazM0IiwiYSI6ImNrdm1iNXd2dTA5NWIycGpwYWJrbWt5Y2kifQ.TtejJBtK0J0XU85fgsO6eg",
+                limit: 1
+            })
+        )
+        .then(response => response.json())
+        .then(data => {
+            console.log("Drop off")
+            console.log(data.features[0].center);
+        })
+    }
+
+    useEffect(() => {
+        getPickupCoordinates();
+        getDropoffCoordinates();
+    }, [])
+    
     return (
         <Wrapper>
             <Map />
